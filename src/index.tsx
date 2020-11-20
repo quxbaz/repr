3b4529a3a14797a97a42f120b1439ec5c397406f
David Yeung <quxbaz@gmail.com>
2020-11-19 20:37:15 -0700

    Stuff.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import css from './style.css'
 import React, {FunctionComponent} from 'react'
 import {render} from 'react-dom'
 
@@ -17,16 +18,57 @@ import {render} from 'react-dom'
 //   number: string,
 // }
 
-const Number = ({number, frame}) => (
-  <div>{frame}</div>
-)
-
-const Mult2 = ({frame}) => {
-  <div>Multiply by 2</div>
+const numberFrameData = {
+  0: {
+    x: 0,
+    y: 0,
+  },
+  10: {
+    x: 100,
+    y: 100,
+  },
+  20: {
+    x: 50,
+    y: 50,
+  },
 }
 
-const Animation = ({frame}) => (
-  <Number number={2} frame={frame} />
+const Animation = ({children, frame, ...props}) => {
+  return (
+    <div {...props}>
+      {children}
+    </div>
+  )
+}
+
+const Number = ({number, frame}) => {
+
+  const nFrames = Object.keys(numberFrameData).map(n => parseInt(n))
+  const low = Math.max(...nFrames.filter(n => n <= frame))
+  const high = Math.min(...nFrames.filter(n => n >= frame))
+
+  const left = (numberFrameData[high].x - numberFrameData[low].x)
+  const top = (numberFrameData[high].y - numberFrameData[low].y)
+
+  const style = {left, top}
+
+  return (
+    <Animation className={css.Number} style={style} frame={frame}>
+      <div>{frame}</div>
+    </Animation>
+  )
+}
+
+const Mult2 = ({frame}) => (
+  <Animation className={css.Func} frame={frame}>
+    <div>Multiply by 2</div>
+  </Animation>
+)
+
+const CustomAnimation = ({frame}) => (
+  <Animation frame={frame}>
+    <Number frame={frame} number={2}/>
+  </Animation>
 )
 
 let frame = 0
@@ -43,7 +85,7 @@ window.addEventListener('keydown', (event) => {
 
 function renderApp () {
   render(
-    <Animation frame={frame} />,
+    <CustomAnimation frame={frame} />,
     document.getElementById('Main')
   )
 }
