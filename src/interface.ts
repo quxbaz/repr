2e80f096a2ee525f8ec5eb16925da1d4bd533094
David Yeung <quxbaz@gmail.com>
2020-11-19 19:30:43 -0700

    Config.

---
+++ src/interface.ts
@@ -0,0 +1,18 @@
+console.log('foobar')
+
+// (animation, frame) -> animationFrame
+
+
+// function mult2 (n) {
+//   return n * 2
+// }
+
+// const input = 3
+// const output = mult2(2)  // -> 6
+
+
+// // function animate (frame) => {
+
+// // }
+
+// // <AddAnimation />
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const abs = (...args) => path.resolve(__dirname, ...args)
+
+module.exports = () => ({
+
+  mode: 'development',
+  // devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: [],
+        include: [
+          abs('node_modules/stateful-router'),
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?{"modules": {"localIdentName": "[path][name]__[local]"}}',
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+})
